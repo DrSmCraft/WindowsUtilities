9f088838a3166849ddc85729b738b761d2c21c3c
Samuil Orlioglu <samuil2000@hotmail.com>
2021-01-06 00:04:02 -0500

    Added CPP files

---
+++ WindowsUtilities.CPP/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <string>
+#include "string_util.h"
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+
+}
---
+++ WindowsUtilities.CPP/string_util.h
@@ -0,0 +1,20 @@
+#include <vector>
+
+#ifndef WINDOWSUTILITIES_STRING_UTIL_H
+#define WINDOWSUTILITIES_STRING_UTIL_H
+
+
+extern std::string
+replace_all(int pos, std::string original, const std::string &to_replace, const std::string &replacement);
+
+extern int count_occurances(int pos, const std::string original, const std::string substring);
+
+extern std::vector<std::string> split_by_substring(std::string original, std::string substring);
+
+extern std::string ltrim(std::string str);
+
+extern std::string rtrim(std::string str);
+
+extern std::string trim(std::string str);
+
+#endif //WINDOWSUTILITIES_STRING_UTIL_H
