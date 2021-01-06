10669549722b77179614c4c32f8eea65e6560d63
s28orli <s28orli@siena.edu>
2021-01-05 23:57:00 -0500

    Added CS Project

---
+++ WindowsUtilities.CS/NetshPassword/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetshPassword
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("NetshPassword");
+        }
+    }
+}
